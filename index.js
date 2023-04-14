50dde4a8a91a4bdaeced2afe8258531c22aa1ad8
Francisco Casillas <franky.casillas09@gmail.com>
2023-04-13 19:37:59 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code in this file!
+const currentUser = 'CISCO';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser}!`;
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
